67de41cab0c6c71e21c6877f7cf786737a2e6787
Konstantin Knopp <k.knopp@das-unikat.com>
2022-05-04 18:51:25 +0200

    initial commit

---
+++ src/ElfFantasyApiServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ElfFantasy\API;
+
+use Illuminate\Support\ServiceProvider;
+
+class ElfFantasyApiServiceProvider extends ServiceProvider
+{
+    public function register()
+    {}
+
+    public function boot()
+    {}
+}
