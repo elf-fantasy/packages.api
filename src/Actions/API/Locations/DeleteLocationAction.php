e90290f545e5c36e3e08ecd0caa01f64c56b40e5
Konstantin Knopp <k.knopp@das-unikat.com>
2022-05-05 09:34:25 +0200

    .

--- src/Actions/API/Locations/DeleteLocationAction.php
+++ src/Actions/API/Locations/DeleteLocationAction.php
@@ -17,7 +17,5 @@ class DeleteLocationAction
             'id'       => $id,
         ]);
         $response = $deleteLocationRequest->send();
-
-        Log::info($response->body());
     }
 }
--- src/Actions/API/Teams/UpdateTeamAction.php
+++ src/Actions/API/Teams/UpdateTeamAction.php
@@ -2,10 +2,8 @@
 
 namespace ElfFantasy\API\Actions\API\Teams;
 
-use ElfFantasy\API\Http\Integrations\DataApi\Requests\Teams\StoreTeamRequest;
 use ElfFantasy\API\Http\Integrations\DataApi\Requests\Teams\UpdateTeamRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class UpdateTeamAction
@@ -14,8 +12,6 @@ class UpdateTeamAction
 
     public function handle(Request $request, $id)
     {
-        Log::info($request->all());
-        Log::info($id);
         $updateTeamRequest = new UpdateTeamRequest((int) $request->get('real_id'));
         $updateTeamRequest->setQuery([
             'id'          => $request->get('real_id'),
